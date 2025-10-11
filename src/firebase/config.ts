afa29d7f6507a39711a2125df31694e224d15f85
Mahvir Beniwal <mahvirbeniwal8@gmail.com>
2025-10-11 13:15:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3466326108-83cc8",
+  "appId": "1:336307476837:web:af1ba678790bbd52eb298c",
+  "apiKey": "AIzaSyA_qm_99FT-q68EgDzVVywh5bCXJHQNG7o",
+  "authDomain": "studio-3466326108-83cc8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "336307476837"
+};
