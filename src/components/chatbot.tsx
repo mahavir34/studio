f839976fe8a39b23259963814d774e40f72300f4
Mahvir Beniwal <mahvirbeniwal8@gmail.com>
2025-10-11 12:58:03 +0000

    सैम परेशानी अभी तक चेंज नहीं हुआ है

--- src/components/chatbot.tsx
+++ src/components/chatbot.tsx
@@ -85,7 +85,7 @@ export function Chatbot() {
         </SheetHeader>
         <div className="flex-1 my-4 pr-4">
             <div className="flex flex-col items-center justify-center h-full text-center">
-                 <img src="https://storage.googleapis.com/project-spark-341015.appspot.com/93a9a797-28e4-4c4f-9c4c-34d9a1f59239" alt="Chatbot mascot" className="w-48 h-48" />
+                 <img src="https://storage.googleapis.com/project-spark-341015.appspot.com/7d4e5a95-4a5f-4d13-92f5-de1618d3b846" alt="Chatbot mascot" className="w-48 h-48" />
             </div>
         </div>
         <ScrollArea className="flex-1 my-4 pr-4" viewportRef={viewportRef}>
